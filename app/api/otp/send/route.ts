5d715628643a05abbd71722c8790018514b1d21e
Mohamad-aminn <mohamadeaminn@gmail.com>
2025-02-15 02:56:30 +0400

    jwt api route

--- app/api/otp/send/route.ts
+++ app/api/otp/send/route.ts
@@ -1,5 +1,6 @@
 import db from "@/app/db/db";
 import { users } from "@/app/db/schema";
+import { phoneSchema } from "@/app/schema/user";
 import { generateOtp } from "@/app/utils/otp";
 import sms from "@/app/utils/sms";
 import { eq } from "drizzle-orm";
@@ -7,14 +8,20 @@ import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
-    const { phone } = await req.json();
+    const { phone }: { phone: string } = await req.json();
+
+    // add zod validation here
+    const validationPhone = phoneSchema.safeParse({ phone });
+    if (!validationPhone.success) {
+      return Response.json(validationPhone.error, { status: 400 });
+    }
 
     const user = await db.query.users.findFirst({
       where: eq(users.phone_number, phone),
     });
 
     if (user?.otp_expiration) {
-      if (new Date().getTime() - user.otp_expiration.getTime() <= 180000) {
+      if (new Date().getTime() - user.otp_expiration.getTime() <= 0) {
         return Response.json({}, { status: 202 });
       }
     }
@@ -26,7 +33,12 @@ export const POST = async (req: NextRequest) => {
     }
     const otp = generateOtp();
 
-    await db.update(users).set({ otp_code: otp, otp_expiration: new Date() });
+    const expirationDate = new Date();
+    expirationDate.setMinutes(expirationDate.getMinutes() + 4);
+
+    await db
+      .update(users)
+      .set({ otp_code: otp, otp_expiration: expirationDate });
     const res = await sms.send(
       phone,
       process.env.FROM,
---
+++ app/api/otp/verify/route.ts
@@ -0,0 +1,49 @@
+import db from "@/app/db/db";
+import { users } from "@/app/db/schema";
+import { loginSchema } from "@/app/schema/user";
+import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { phone, otp }: { phone: string; otp: string } = await req.json();
+
+    // zod
+    const validate = loginSchema.safeParse({ phone, otp });
+    if (!validate.success) {
+      return Response.json(validate.error, { status: 400 });
+    }
+
+    const user = await db.query.users.findFirst({
+      where: eq(users.phone_number, phone),
+    });
+    if (!user) {
+      return Response.json(
+        { error: "یوزری با این شماره درخواست کد نکرده!" },
+        { status: 400 }
+      );
+    }
+
+    if (new Date().getTime() - user.otp_expiration!.getTime() >= 0) {
+      return Response.json(
+        { error: "وقت کد تموم شده کد جدید بگیرید" },
+        { status: 400 }
+      );
+    }
+
+    // check otp and gg
+    if (otp !== user.otp_code) {
+      return Response.json({ error: "کد اشتباه!" }, { status: 400 });
+    }
+
+    const jwtToken = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET!
+    );
+
+    return Response.json({ access_token: jwtToken }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: "server error" }, { status: 500 });
+  }
+};
