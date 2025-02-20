97c936ae1961e0050a6c7de400737b0fe38a3f19
Mohamad-aminn <mohamadeaminn@gmail.com>
2025-02-20 20:44:07 +0400

    sidebar links

---
+++ app/(DASHBOARD)/dashboard/components/SidabarLinks.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { GrTransaction } from "react-icons/gr";
+
+const sidebarLinks = [
+  {
+    icon: <GrTransaction />,
+    name: "خانه",
+    href: "/dashboard",
+  },
+  {
+    icon: <GrTransaction />,
+    name: "سفارش ها",
+    href: "/dashboard/transactions",
+  },
+  {
+    icon: <GrTransaction />,
+    name: "خروج",
+    href: "/dashboard/logout",
+  },
+];
+const SidabarLinks = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  return (
+    <>
+      {sidebarLinks?.map((l, i: number) => (
+        <Link
+          key={i}
+          href={l.href}
+          className={`flex relative items-center gap-5 px-8 me-8 py-4 rounded-e-lg ${
+            pathname === l.href && "bg-[#0F151A]"
+          }`}
+        >
+          {pathname === l.href && (
+            <div className="absolute w-1 h-full bg-blue-700 right-0 top-0"></div>
+          )}
+          {l.icon}
+          <p>{l.name}</p>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default SidabarLinks;
--- app/(DASHBOARD)/dashboard/components/Sidebar.tsx
+++ app/(DASHBOARD)/dashboard/components/Sidebar.tsx
@@ -2,6 +2,10 @@ import db from "@/app/db/db";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 import React from "react";
+import SidabarLinks from "./SidabarLinks";
+
+const activeLink = "#0F151A";
+const content = "#0F151A";
 
 const Sidebar = async () => {
   const jwt = await getCookie("access_token", { cookies });
@@ -16,8 +20,13 @@ const Sidebar = async () => {
   const user = await res.json();
 
   return (
-    <div className="w-80 min-h-dvh bg-slate-800">
-      <div>{user.phone_number}</div>
+    <div className="w-80 min-h-dvh bg-[#16202A] py-7">
+      <div className="size-full flex flex-col">
+        <div className="font-bold text-6xl text-center mb-20">Nova</div>
+        <div className="flex flex-col gap-4">
+          <SidabarLinks />
+        </div>
+      </div>
     </div>
   );
 };
